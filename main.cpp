3f2fef02783e9be92c8a3a71665cb9a870d8f8dc
LowSwoo <lowswoo@gmail.com>
2021-10-14 22:54:18 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+struct tableString {
+    char nein;
+    char ternary[3];
+};
+
+const int LEN = 80;
+
+const tableString table[9] = {
+    {'0', "00"},    
+    {'1', "01"},
+    {'2', "02"},
+    {'3', "10"},
+    {'4', "11"},
+    {'5', "12"},
+    {'6', "20"},
+    {'7', "21"},
+    {'8', "22"}
+};
+
+int main(){
+    char ch[LEN];
+    for (int i = 0; i <= LEN; i++){
+        ch[i] = 0;
+    }
+    cin.getline(ch,LEN);
+    for (int i =0; i<= LEN; i++){
+        if (ch[i]){
+            cout << ch[i] << endl;
+        }
+    }
+
+    
+    return 0;
+}
