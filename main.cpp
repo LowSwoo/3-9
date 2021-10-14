8ca69725d753e7f2d68ebc76a913de4c8d861084
LowSwoo <lowswoo@gmail.com>
2021-10-15 00:48:38 +0300

    end

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <cstring>  
+#include <string> 
 
 using namespace std;
 
-struct tableString {
+struct tableString
+{ // Структура, содержащая информацию о типе данных для перевода
     char nein;
     char ternary[3];
 };
@@ -10,7 +13,7 @@ struct tableString {
 const int LEN = 80;
 
 const tableString table[9] = {
-    {'0', "00"},    
+    {'0', "00"},
     {'1', "01"},
     {'2', "02"},
     {'3', "10"},
@@ -18,23 +21,62 @@ const tableString table[9] = {
     {'5', "12"},
     {'6', "20"},
     {'7', "21"},
-    {'8', "22"}
-};
+    {'8', "22"}};
 
-xyu
+int main()
+{
+    char ch[LEN]; // Объявление массива
+    char res[LEN / 2];
+    char tmp[2];
+    int count = 0; // Подсчёт колличества символов
+    int count_1 = 0; // Подсчёт колличества символов
 
-int main(){
-    char ch[LEN];
-    for (int i = 0; i <= LEN; i++){
-        ch[i] = 0;
+    for (int i = 0; i <= LEN; i++)
+    {              //Зачистка массива нулями
+        ch[i] = 0; //Зачистка массива нулями
     }
-    cin.getline(ch,LEN);
-    for (int i =0; i<= LEN; i++){
-        if (ch[i]){
-            cout << ch[i] << endl;
+    cin.getline(ch, LEN);          // Считывание данных с клавиатуры
+    for (int i = 0; i <= LEN; i++) //Считаем символы и выводим их на экран
+    {
+        if (ch[i])
+        {
+            count++;
+            cout << i << ": " << ch[i] << endl;
         }
     }
-
-    
+    if ((count % 2) != 0) // При нечётном кол-ве символов добавляем в начало 0
+    {
+        for (int i = count - 1; i >= 0; i--)
+        {
+            ch[i + 1] = ch[i];
+        }
+        ch[0] = '0';
+    }
+    cout << endl;
+    for (int i = 0; i <= LEN; i++)
+    {
+        if (ch[i])
+        {
+            cout << i << ": " << ch[i] << endl;
+        }
+    }
+    count_1 = 0;
+    for (int i = 0; i <= count; i+=2) {
+        tmp[0] = ch[i];
+        tmp[1] = ch[i + 1];
+        for (int j = 0; j< 9; j++) {
+            // cout << "table:" << table[j].ternary  << " tmp:" << tmp << " tmp == table: " << ((tmp[0] == table[j].ternary[0]) and (tmp[1] == table[j].ternary[1])) << endl;
+            if ((tmp[0] == table[j].ternary[0]) and (tmp[1] == table[j].ternary[1])){
+                res[count_1] = table[j].nein;
+                // cout << "strcmp" << endl << res[count] << endl;
+                count_1++;
+            }
+        }
+    }
+    cout << endl;
+    for (int i =0; i <= count; i++){
+        cout  << res[i];
+    }
+    cout << endl;
     return 0;
 }
